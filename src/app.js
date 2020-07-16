c5a188941d2405c72189b74a51a731a91dbe64db
frncsdrk <frncsdrk@protonmail.com>
2020-07-17 00:28:10 +0200

    Add append html adapter

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const { r } = require('renaissance')
+const AppendHtmlAdapter = require('renaissance-append-html')
+
 const Utils = require('./plugins/Utils')
 const LocalStorage = require('./adapters/LocalStorage')
 const HelloWorld = require('./components/HelloWorld')
@@ -6,12 +8,14 @@ const HelloWorldPlugin = require('./plugins/hello-plugin')
 
 const utilsPlugin = r.registerPlugin('utils', Utils);
 const helloPlugin = r.registerPlugin('hello', HelloWorldPlugin);
+const aha = r.registerAdapter('appendHTML', AppendHtmlAdapter);
 const lsa = r.registerAdapter('localStorage', LocalStorage);
 
 // plugin
 r.hello.hello();
 
 // adapter
+aha.appendHTML('<p>appendHTML</p>', r.utils.getNodes('#renaissance')[0], function () { console.log('AHA') })
 console.log(r.utils.getNodes('#renaissance'));
 lsa.save('foo', 'bar');
 console.log(lsa.get('foo'));
