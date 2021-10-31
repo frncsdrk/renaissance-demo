280c9d250cce9221495a9b99d01da5a9d9d56e3b
frncsdrk <frncsdrk@protonmail.com>
2021-10-31 10:25:46 +0100

    Add renaissance-spa to demo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const { r } = require('renaissance')
 const AppendHtmlAdapter = require('renaissance-append-html')
+const SpaAdapter = require('renaissance-spa')
 
 const Utils = require('./plugins/Utils')
 const LocalStorage = require('./adapters/LocalStorage')
@@ -9,6 +10,7 @@ const HelloWorldPlugin = require('./plugins/hello-plugin')
 const utilsPlugin = r.registerPlugin('utils', Utils);
 const helloPlugin = r.registerPlugin('hello', HelloWorldPlugin);
 const aha = r.registerAdapter('appendHTML', AppendHtmlAdapter);
+const spa = r.registerAdapter('spa', SpaAdapter)
 const lsa = r.registerAdapter('localStorage', LocalStorage);
 
 // plugin
@@ -19,6 +21,11 @@ aha.appendHTML('<p>appendHTML</p>', r.utils.getNodes('#renaissance')[0], functio
 console.log(r.utils.getNodes('#renaissance'));
 lsa.save('foo', 'bar');
 console.log(lsa.get('foo'));
+
+// spa
+spa.register('hello-world', '#hello-world')
+spa.register('hello-spa', '#hello-spa')
+spa.goto('hello-world')
 
 // component
 const hw = new HelloWorld();
